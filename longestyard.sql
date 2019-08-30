7e3db4b9f48517dbe1ddb3bf9a7c95fa06837380
Lenny Hughes <lenaudhughes@gmail.com>
2019-08-29 20:44:05 -0400

    Run this in PG Admin to create database and player table

---
+++ longestyard.sql
@@ -0,0 +1,76 @@
+--  DROP Database IF EXISTS "LongestYardDB";
+
+-- CREATE DATABASE "LongestYardDB"
+--     WITH 
+--     OWNER = postgres
+--     ENCODING = 'UTF8'
+--     LC_COLLATE = 'English_United States.1252'
+--     LC_CTYPE = 'English_United States.1252'
+--     TABLESPACE = pg_default
+--     CONNECTION LIMIT = -1;
+
+-- Create Table Player (
+-- 	playerID serial unique primary key,
+-- 	position varchar(5) Not Null,
+-- 	firstName varchar (50) Not Null,
+-- 	lastName varchar (75) Not Null,
+-- 	av smallint Not Null,
+-- 	picture varchar (255),
+-- 	apiDataAvailable boolean
+-- )
+
+-- Alter table player alter column apiDataAvailable
+-- set default true;
+
+-- Alter table player 
+-- rename column picture to playerPic;
+
+-- Alter table player
+-- add column mugshotpic varchar (255);
+
+
+-- insert into player (position,firstName,lastName,av)
+-- values
+-- ('QB','Ben','Roethlisberger',185),
+-- ('QB','Johnny','Manziel',5),
+-- ('QB','Chad','Kelly',0),
+-- ('QB','Steve','McNair',126),
+-- ('QB','Jake','Plummer',92),
+-- ('QB','Daunte','Culpepper',97),
+-- ('QB','Brian','Griese',61),
+-- ('QB','Michael','Vick',113),
+-- ('RB','OJ','Simpson',116),
+-- ('RB','Jim','Brown',122),
+-- ('RB','Leonard','Fournette',11),
+-- ('RB','Aaron','Jones',9),
+-- ('RB','LeVeon','Bell', 60),
+-- ('RB','LeGarrette','Blount', 46),
+-- ('RB','Ray','Rice',70),
+-- ('RB','Marshawn','Lynch',90),
+-- ('RB','Adrian','Peterson',115),
+-- ('RB','Ricky','Williams', 90),
+-- ('WR','Demaryius','Thomas',78),
+-- ('WR','Robby','Anderson', 17),
+-- ('WR,','Josh','Gordon', 28),
+-- ('WR','Chad','Johnson',104),
+-- ('WR','Dez','Bryant',71),
+-- ('WR','Julian','Edelman',59),
+-- ('WR','Hines','Ward',118),
+-- ('WR','Donte','Stallworth',45),
+-- ('WR','Plaxico','Burress',83),
+-- ('WR','Brandon','Marshall',105),
+-- ('WR','Randy','Moss', 161),
+-- ('WR','Andre','Rison', 99),
+-- ('TE','Aaron','Hernandez',23),
+-- ('TE','Kellen','Winslow Jr.', 43),
+-- ('TE','Fred','Davis',15),
+-- ('K','Josh','Brown',46),
+-- ('K','Matt','Prater',34),
+-- ('K','Sebastian','Janikowski', 60),
+-- ('K','Jeff','Reed',25);
+
+-- update player 
+--   set apidataavailable = false
+--   where playerID in (1,9,10
+
+-- select * from player
