fe2ab51e026cf5515ab29e81aebe0eacabf7da68
Campbell <mikecampbell@quickenloans.com>
2019-09-04 21:54:20 -0400

    added first name and last name

--- Client/LongestYard/src/app/interfaces.ts
+++ Client/LongestYard/src/app/interfaces.ts
@@ -1,15 +1,16 @@
 export interface Player {
-    name: string; // From Database
+    firstName: string; // From Database
+    lastName: string; // From Database
     postion: string; // From Database
     picture: string; // From internal folder - also has a field in the database
     mugshot?: string; // From internal folder - also has a field in the database
-    offenses: string[]; // From API call
+    offenses?: string[]; // From API call
     av: number; // From database
     badScore: number; // Uses data from API to calculate based on internal logic
-    careerStat1: string; 
-    careerStat2: string;
-    careerStat3: string;
-    careerStat4?: string;
-    careerStat5?: string;
-    careerBlurb: string;
+    careerStat1: string;  // Hard Coded
+    careerStat2: string; // Hard Coded
+    careerStat3: string; // Hard Coded
+    careerStat4?: string; // Hard Coded
+    careerStat5?: string; // Hard Coded
+    careerBlurb: string; // Hard Coded
 }
