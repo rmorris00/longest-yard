c9f05c999fe05213a26803ea0b0c78c168d1573d
Campbell <mikecampbell@quickenloans.com>
2019-09-14 20:38:50 -0400

    added bootstrap

--- Server/pg-connection-pool.js
+++ Server/pg-connection-pool.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 const credentials = new Pool({
 user: "postgres",
-password: "password",
+password: "mcampbel2",
 host: "localhost",
 port: 5432,
 database: "LongestYardDB",
