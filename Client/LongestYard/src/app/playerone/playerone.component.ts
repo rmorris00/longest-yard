21b681de9bfe7405eb01fcb7e34d3b00999e0ba1
Campbell <mikecampbell@quickenloans.com>
2019-09-16 15:23:15 -0400

    updated CSV with working img url, added dynamic player cards

--- Client/LongestYard/src/app/playerone/playerone.component.ts
+++ Client/LongestYard/src/app/playerone/playerone.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BuildObjectService } from '../build-object.service';
+import { Player } from '../interfaces'
 
 @Component({
   selector: 'app-playerone',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayeroneComponent implements OnInit {
 
-  constructor() { }
+  availablePlayers: Player[];
+
+  constructor(private buildObject: BuildObjectService) { }
 
   ngOnInit() {
+    this.availablePlayers = this.buildObject.getPlayerList();
   }
 
 }
